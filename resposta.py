78d0ae555b6a195aade2e97f450eae2c351b60b1
Gabriel Leal <marqsleal@gmail.com>
2024-10-02 02:01:29 -0300

    Comparador e Modo Debug

--- resposta.py
+++ resposta.py
@@ -2,18 +2,18 @@ from jaccard import jaccard_index
 from levenshtein import levenshtein_distance
 from regex import normalizar_texto
 
-def comparador_sim(similaridade_jaccard: float, distancia_levenshtein: float, threshold: float,
-                   melhor_similaridade: float) -> float:
-    if (similaridade_jaccard > melhor_similaridade
-        and similaridade_jaccard > threshold) or (distancia_levenshtein > melhor_similaridade
-                                                  and distancia_levenshtein > threshold):
-        if similaridade_jaccard > distancia_levenshtein:
-            return similaridade_jaccard
-        else:
-            return distancia_levenshtein
+threshold_jaccard = 0.15
+threshold_levenshtein = 10.0
+
+def comparador_sim(indice_jaccard: float, distancia_levenshtein: float, melhor_similaridade: float,
+                   pergunta_usuario_norm: str, pergunta_norm: str) -> float:
+    if indice_jaccard > melhor_similaridade and indice_jaccard >= threshold_jaccard:
+        return indice_jaccard
+    elif distancia_levenshtein <= threshold_levenshtein:
+        return 1 - (distancia_levenshtein / max(len(pergunta_usuario_norm), len(pergunta_norm)))
     return melhor_similaridade
 
-def escolhe_resposta(pergunta_usuario: str, perguntas_respostas: dict, threshold=0.5) -> str:
+def escolhe_resposta(pergunta_usuario: str, perguntas_respostas: dict, debug=False) -> str:
     pergunta_usuario_norm = normalizar_texto(pergunta_usuario)
 
     melhor_pergunta = None
@@ -22,16 +22,23 @@ def escolhe_resposta(pergunta_usuario: str, perguntas_respostas: dict, threshold
     for pergunta, resposta in perguntas_respostas.items():
         pergunta_norm = normalizar_texto(pergunta)
 
-        similaridade_jaccard = jaccard_index(pergunta_usuario_norm, pergunta_norm)
+        indice_jaccard = jaccard_index(pergunta_usuario_norm, pergunta_norm)
         distancia_levenshtein = levenshtein_distance(pergunta_usuario_norm, pergunta_norm)
 
-        comparador = comparador_sim(similaridade_jaccard, distancia_levenshtein, threshold, melhor_similaridade)
+        comparador = comparador_sim(indice_jaccard, distancia_levenshtein, melhor_similaridade, pergunta_usuario_norm, pergunta_norm)
 
-        if comparador > melhor_similaridade and comparador > threshold:
+        if debug:
+            print(
+                f"""
+                Pergunta: {pergunta}, Jaccard: {indice_jaccard}, Levenshtein: {distancia_levenshtein}, 
+                \nComparador: {comparador}, Melhor Similaridade: {melhor_similaridade}
+                """)
+
+        if comparador > melhor_similaridade:
             melhor_similaridade = comparador
             melhor_pergunta = pergunta
 
     if melhor_pergunta:
         return perguntas_respostas[melhor_pergunta]
     else:
-        return 'Desculpe, não entendi sua pergunta!'
+        return "Desculpe, não entendi sua pergunta."
